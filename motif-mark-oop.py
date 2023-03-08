9bcefa219b45a73f466de4e8e0a53b754610be57
Christian La France <christianlafrance7@gmail.com>
2023-03-08 13:39:32 -0800

    added more descriptive comments and docstrings

--- motif-mark-oop.py
+++ motif-mark-oop.py
@@ -6,6 +6,13 @@ import argparse
 from bioinfo import onelineFasta
 
 class Gene:
+
+    """
+    The Gene class is used to create gene objects from FASTA sequences. One Gene object
+    is created for each FASTA sequence. Two methods, getExonPos and findMotifs, are used
+    to retrieve relevant sequence info. These methods return None, but set relevant 
+    attributes for each Gene instance. 
+    """
     
     def __init__(self, header:str, seq:str):
         self.header = header # the fasta header
@@ -14,10 +21,11 @@ class Gene:
         self.name = header.split(" ")[0][1:] # the gene name from the header
 
 
-    def getExonPos(self):
+    def getExonPos(self) -> None:
         '''
         Parses a gene sequence, searches for the exons which are in all caps,
-        and returns the 1-based start position of each exon. Returns a dictionary
+        and returns the 1-based start position of each exon. Returns None but sets
+        the exonPos attribute of the Gene object. This is a dictionary
         with exon number as the key and a tuple (start pos, end pos) as the value.
         Positions are in 1-based numbering. 
         '''
@@ -43,11 +51,13 @@ class Gene:
         return
 
 
-    def findMotifs(self, motifList: list):
+    def findMotifs(self, motifList: list) -> None:
         """
         Parses the gene sequence for instances of a motif. A list of motif sequences
-        are passed into the function. Saves the positions of each motif found as 
-        an attribute of the Gene class. 
+        are passed into the function. Returns None but sets the motifs attribute of 
+        the Gene object. This attribute is a dictionary where the motif sequence is
+        the key and the value is a list of tuples representing the (start, end) 
+        positions of each occurance of that motif. Positions are in 1-based numbering. 
         """
         self.motifs = {}
         for motif in motifList:
@@ -64,13 +74,16 @@ class Gene:
 
 class GeneDraw:
 
+    """
+    Class used to create the GeneDraw object which is used to draw out the pycairo images.
+    Has no attributes but uses two methods to generate the png gene image based on the data
+    in the Gene objects. 
+    """
     
-    def findMaxSeqLen(self, genes: list):
+    def findMaxSeqLen(self, genes: list) -> None:
         """
-        Arguments:
-        A list of Gene objects. 
-        Finds the length of the longest sequence and returns the result
-        as an integer. 
+        Iterates over a list of Gene objects and finds the length of the longest 
+        sequence and returns None but sets the max_seq_len attribute for the object. 
         """
         max_len = 0
         for gene in genes:
@@ -78,6 +91,8 @@ class GeneDraw:
                 max_len = gene.seq_len
         
         self.max_seq_len = max_len
+
+        return
 
 
     def drawGene(self, genes: list, filename:str, motifs:list): 
@@ -97,11 +112,13 @@ class GeneDraw:
         n_motifs = len(motifs) # total number of motifs. Used to adjust png dims for legend. 
         png_len = n_genes * 100 + n_motifs*40 # adjusts the length of the png for number of genes. 
 
+        # This list of R, G, B values will be used to color each motif in the drawing. 
         colors = [(0,255,0), (0,0,255), (255,255,0), (0,255,255), (255,0,255), 
                 (192,192,192), (128,0,0), (128,128,0), (0,128,0), (128,0,128)]
         
         legend = {} # stores motifs and their colors for the figure legend. 
 
+        # initialize the drawing
         with cairo.SVGSurface(f"{file_prefix}.svg", self.max_seq_len, png_len) as surface:
 
             # fill background
@@ -112,7 +129,6 @@ class GeneDraw:
 
             n = 50 # used for spacing genes. 
             for gene in genes:
-
                 # add gene name as text
                 context.set_source_rgb(0, 0, 0)
                 context.set_font_size(15)
@@ -120,26 +136,27 @@ class GeneDraw:
                 context.move_to(0, n-20)
                 context.show_text(gene.name)
 
-                # darw 1 line per gene
+                # darw 1 line per gene, representing introns. 
                 context.set_source_rgb(0,0,0)
                 context.set_line_width(1)
                 context.move_to(0,n)        #(x,y)
                 context.line_to(gene.seq_len,n)
                 context.stroke()
 
-                # draw 1 rectangle per exon
+                # draw 1 rectangle per exon for each gene. 
                 for position in gene.exonPos.values():
                     context.set_source_rgb(0,0,0)
                     start = position[0]
                     end = position[1]
                     length = end-start
-                    context.rectangle(start,n-20,length,40)        #(start x pos, top y pos, x length, height)
+                    context.rectangle(start,n-20,length,40)       
                     context.stroke()
 
-                # draw 1 rectangle per motif
-                color = 0
-    
+                # draw 1 rectangle per motif. 
+                color = 0 # used to cycle through the colors list. 
                 for motif_name in gene.motifs.keys():
+
+                    # keep track of what color was used for each motif in legend dict.
                     if motif_name not in legend:
                         r, g, b = colors[color]
                         context.set_source_rgb(r, g, b)
@@ -157,14 +174,13 @@ class GeneDraw:
 
                 n += 100
 
-
             # add legend
-
             # add legend text
             context.move_to(0, n)
             context.set_source_rgb(0, 0, 0)
             context.show_text("Motif sequence | Color")
 
+            # add motif sequences and corresponding colors to legend. 
             for motif in legend.keys():
                 n += 20
                 context.set_source_rgb(0, 0, 0)
@@ -175,20 +191,25 @@ class GeneDraw:
 
                 r, g, b = legend[motif]
                 context.set_source_rgb(r, g, b)
-                x_start_pos = len(max(legend.keys(), key=len))*11 # adjust for seq length
+                x_start_pos = len(max(legend.keys(), key=len))*13 # adjust for seq length
                 context.rectangle(x_start_pos, n-10,10,10)        #(start x pos, top y pos, x length, height)
                 context.stroke()
-
 
             surface.write_to_png(f"{file_prefix}.png")
 
         return 
 
+
 def get_args():
     """
     Parse command line for arguments and return. 
     """
-    parser = argparse.ArgumentParser(description="")
+    parser = argparse.ArgumentParser(description="""
+    Parses a FASTA file and motif sequence file and draws a line and box representation of
+    introns and exons with the location of each motif. Motifs are color coded. The size of
+    each intro, exon, and motif is drawn to scale. Assumes that introns are in lower case
+    and exons are in upper case in the FASTA file.
+    """)
     parser.add_argument("-f", help="fasta file", required=True, type=str)
     parser.add_argument("-m", help="motif file", required=True, type=str)
     return parser.parse_args()
@@ -206,7 +227,6 @@ def readFiles(fasta: str, motif_file:str) -> tuple[dict,list]:
     returns a tuple storing reads (dictionary of headers and sequences),
     and motifs (list of motif sequences).
     """
-
     reads = onelineFasta(fasta)
     with open(motif_file, "r") as mf:
         motifs = [line.rstrip('\n') for line in mf]
@@ -216,13 +236,14 @@ def readFiles(fasta: str, motif_file:str) -> tuple[dict,list]:
 
 
 
-
-
-
 ################# Function calls ###########################
 
+# Parse the FASTA and motifs files. 
 seq_dict, motifs = readFiles(args.f, args.m)
 
+
+# instantiate a gene object for each read in the FASTA file using the Gene class. 
+# store each gene object in a list. 
 gene_objects = []
 for read in seq_dict.keys():
     gene = Gene(read, seq_dict[read])
@@ -231,8 +252,11 @@ for read in seq_dict.keys():
     gene.findMotifs(motifs)
 
 
+# instantiate the plotting object. 
 plot_object = GeneDraw()
 plot_object.findMaxSeqLen(gene_objects)
 
+
+# use the drawGene method to draw each gene and the motifs. 
 plot_object.drawGene(gene_objects, args.f, motifs)
 
